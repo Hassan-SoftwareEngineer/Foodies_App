70d50a591a35d534d896cfd1da925de2c37e673c
Hassan Javed <hassan.javed.softwareengineer@gmail.com>
2024-12-25 00:39:00 +0500

    update and complete meals and mealDeatils Page

--- app/meals/[meal_id]/page.js
+++ app/meals/[meal_id]/page.js
@@ -1,10 +1,38 @@
-import React from 'react'
-
-const MealDetails = () => {
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+const MealDetails = ({params}) => {
+  const meal_id = params.meal_id;
+  const meal = getMeal(meal_id);
+  if(!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
-    <div>
-      MealDetail
+   <>
+   <header className={classes.header}> 
+    <div className={classes.image}>
+      <Image src={meal.image} alt={meal.title} fill />
     </div>
+    <div className={classes.headerText}>
+      <h1> {meal.title} </h1>
+      <p className={classes.creator}> 
+        by <a href={`mailto:${meal.creator_email}`}> 
+         {meal.creator}</a>
+      </p>
+      <p className={classes.summary}>{meal.summary}</p>
+    </div>
+   </header>
+   <main> 
+    <p className={classes.instructions} 
+    dangerouslySetInnerHTML={{
+      __html : meal.instructions
+    }}
+    > 
+       </p>
+   </main>
+   </>
   )
 }
 
